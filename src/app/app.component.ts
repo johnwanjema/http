ee4e75261515ae404ebcc9dbee3bf79dc4fcd82a
johnwanjema <jonwanjema@gmail.com>
2019-06-04 22:40:06 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Quote } from './quote-class/quote';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hype';
+  quote: Quote;
+
+  constructor( private http: HttpClient) {
+    
+  }
+
+
+  ngOnInit() {
+
+    interface ApiResponse {
+      quote: string;
+      author: string
+    }
+    this.http.get<ApiResponse>("http://quotes.stormconsultancy.co.uk/random.json").subscribe(data => {
+      this.quote = new Quote(data.quote, data.author)
+    }, err => {
+      this.quote = new Quote("Never, never, never give up.", "winston churchill")
+      alert("Error occured ")
+    })
+  }
+
 }
+
